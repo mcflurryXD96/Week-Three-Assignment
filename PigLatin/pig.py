c2f3251f1c7fc7f8b60a961759bbf17e7670257c
mcflurryxd96 <daniel.mcmurray@myemail.indwes.edu>
2015-09-23 18:45:46 +0000

    changes

--- PigLatin/pig.py
+++ PigLatin/pig.py
@@ -1,3 +1,7 @@
 # Daniel McMurray
 # pig.py
 # English to PigLatin Word Translation
+
+# Prompt the user to enter an English word to translate
+
+word = input("Enter an English word to translate: ")
--- StarWars/star.py
+++ StarWars/star.py
@@ -1,3 +1,14 @@
 # Daniel McMurray
 # starwars.py
 # Week 3 Star Wars Name Translation
+
+# Prompt User for first name, last name, mother's maiden name, and name of birth City
+
+firstname = input("Enter your first name: ")
+lastname = input("Enter your last name: ")
+maiden = input("Enter your mother's maiden name: ")
+city = input("Enter the name of the city in which you were born: ")
+
+# Calculate and print the user's Star Wars name
+
+print(lastname[0:3]+firstname[0:2], maiden[0:2]+city[0:3])
