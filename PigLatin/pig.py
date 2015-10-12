24c76dadcc16c1927455564f4c168f3ffb09d671
mcflurryxd96 <daniel.mcmurray@myemail.indwes.edu>
2015-10-12 15:38:17 +0000

    Changes_after_email

--- PigLatin/pig.py
+++ PigLatin/pig.py
@@ -5,3 +5,12 @@
 # Prompt the user to enter an English word to translate
 
 word = input("Enter an English word to translate: ")
+
+# Program determines whether first letter is a vowel or a consonant
+
+vowel = "aAeEiIoOuU"     # This fixes the case sensitivity issue with words starting with vowels only
+
+if word[0] in vowel:
+    print(word+"yay")
+else:
+    print(word[1:]+word[0]+"ay")
